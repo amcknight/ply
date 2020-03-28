b3be4e369ab9d3693459282a072918c59f213f01
Andrew McKnight <thedonk@gmail.com>
2020-03-28 09:08:38 -0700

    Dump more of the initial commit. Not working

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = select ("first_name", "age") $ from "people.csv"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Lib
-    ( someFunc
+    ( from
+    , select
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.ByteString.Lazy
+import Data.Csv (decode, HasHeader(..))
+
+from :: String -> From
+from path = do
+  csvData <- Data.ByteString.Lazy.readFile path
+  case decode HasHeader csvData of
+    Left err -> ["BAD"]
+    Right csv -> ["Good", "Stuff"]
+
+
+select :: From -> Columns -> Select
+select from columns = (columns, [["YAY"]])
+
+type Columns = [String]
+type From = Columns
+type Rows = (Columns, [[String]])
+type Select = Rows
