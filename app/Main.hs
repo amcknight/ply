ccbe7c6264ddfad4ee80efcf0db8557e32e1cf7c
Andrew McKnight <thedonk@gmail.com>
2020-04-01 11:59:48 -0700

    Modified executable to be called csvsql and did some pretty printig

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,19 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import Printer
-import Loader
 import QueryBuilder
+import Loader
+import Printer
 
 main :: IO ()
 main = do
--- TODO: Read query from CLI
--- Parse the query into tokens or directly to an SQL query representation or emit errors if ill-formatted
-  let query = build "SELECT first_name, age FROM people WHERE age = 35"
--- Validate the structure or emit errors if ill-formatted
--- Read the file row by row, emitting answer rows
+  let queryString = "\
+    \SELECT first_name, age\n\
+    \FROM people\n\
+    \WHERE age = 35"
+  putStrLn queryString
+  putStrLn "----"
+  let query = build queryString
   csvData <- BL.readFile $ "test/rsrc/" ++ table query ++ ".csv"
   putStrLn $ case loadCsv csvData of Left err -> err
                                      Right row -> rowsToMessage row
