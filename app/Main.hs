54af12f1d16d4284e5213cd13fc18b73a343e3eb
Andrew McKnight <thedonk@gmail.com>
2020-04-01 10:54:56 -0700

    Made table avaiable from Query

--- app/Main.hs
+++ app/Main.hs
@@ -13,15 +13,11 @@ main = do
 -- Parse the query into tokens or directly to an SQL query representation or emit errors if ill-formatted
   let q = build "SELECT first_name, age FROM people WHERE age = 35"
 -- Validate the structure or emit errors if ill-formatted
---  from "people" $ where [EqClause "age", 35] $ select [As "first_name" "name", "age"]
 -- Read the file row by row, emitting answer rows
-  csvData <- BL.readFile $ "test/rsrc/" ++ "people_no_header" ++ ".csv"
+  csvData <- BL.readFile $ "test/rsrc/" ++ table q ++ ".csv"
   putStrLn $ loadCsv csvData
 
 loadCsv :: BL.ByteString -> String
 loadCsv csvData = case decode NoHeader csvData of
   Left err -> err
   Right v -> rowsToMessage $ V.toList v
-
-select :: [String] -> String -> [String]
-select = undefined
--- src/QueryBuilder.hs
+++ src/QueryBuilder.hs
@@ -1,18 +1,22 @@
 module QueryBuilder
   ( build
+  , table
   ) where
 
 -- TODO: This needs to work with:
--- Multiple From Strings
--- GROUPBY, HAVING, LIMIT
+-- Multiple From Strings or Join
+-- include JOIN, GROUP BY, HAVING, LIMIT, ORDER BY
 -- Multiple primitives rather than just String and Int (preferably as a param)
 data Col = ICol String | SCol String
 data EqClause = EqCC Col Col | EqCI Col Int | EdCS Col String | EqIC Int Col | EqSC String Col
 newtype Select = Select [Col]
 newtype From = From String
 newtype Where = Where [EqClause]
-data SelectFromWhere = SelectFromWhere Select From Where
+data Query = SelectFromWhere Select From Where
 
 -- TODO: Use the input param to build the query
-build :: String -> SelectFromWhere
-build _ = SelectFromWhere (Select [SCol "first_name", ICol "age"]) (From "people") (Where [EqCI (ICol "age") 35])
+build :: String -> Query
+build _ = SelectFromWhere (Select [SCol "first_name", ICol "age"]) (From "people_no_header") (Where [EqCI (ICol "age") 35])
+
+table :: Query -> String
+table (SelectFromWhere _ (From t) _) = t
