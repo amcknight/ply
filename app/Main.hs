57f3727ad14027e93f7334bc2b19274c171e1822
Andrew McKnight <thedonk@gmail.com>
2020-04-01 11:30:22 -0700

    Factored Main and Lib into Loader and Printer

--- app/Main.hs
+++ app/Main.hs
@@ -1,23 +1,17 @@
 module Main where
 
 import qualified Data.ByteString.Lazy as BL
-import Data.Csv
-import Data.List
-import qualified Data.Vector as V
-import Lib
+import Printer
+import Loader
 import QueryBuilder
 
 main :: IO ()
 main = do
 -- TODO: Read query from CLI
 -- Parse the query into tokens or directly to an SQL query representation or emit errors if ill-formatted
-  let q = build "SELECT first_name, age FROM people WHERE age = 35"
+  let query = build "SELECT first_name, age FROM people WHERE age = 35"
 -- Validate the structure or emit errors if ill-formatted
 -- Read the file row by row, emitting answer rows
-  csvData <- BL.readFile $ "test/rsrc/" ++ table q ++ ".csv"
-  putStrLn $ loadCsv csvData
-
-loadCsv :: BL.ByteString -> String
-loadCsv csvData = case decode NoHeader csvData of
-  Left err -> err
-  Right v -> rowsToMessage $ V.toList v
+  csvData <- BL.readFile $ "test/rsrc/" ++ table query ++ ".csv"
+  putStrLn $ case loadCsv csvData of Left err -> err
+                                     Right row -> rowsToMessage row
--- src/Printer.hs
+++ src/Printer.hs
@@ -1,4 +1,4 @@
-module Lib
+module Printer
   ( rowsToMessage
   ) where
 
