9d4cd0273317438f58cf9f63ac39030bbef3adba
Andrew McKnight <thedonk@gmail.com>
2020-04-01 19:42:13 -0700

    Adding Col type list to the FROM statement to avoid dealing with heterogeneous lists of columns

--- src/QueryBuilder.hs
+++ src/QueryBuilder.hs
@@ -10,12 +10,13 @@ module QueryBuilder
 data Col = ICol String | SCol String
 data EqClause = EqCC Col Col | EqCI Col Int | EdCS Col String | EqIC Int Col | EqSC String Col
 newtype Select = Select [Col]
-newtype From = From String
+data From = From String [Col]
 newtype Where = Where [EqClause]
 data Query = SelectFromWhere Select From Where
 
+-- TODO: Will need the headers for matching Cols
 build :: String -> Query
-build str = SelectFromWhere (Select [SCol "first_name", ICol "age"]) (From "people_no_header") (Where [EqCI (ICol "age") 35])
+build str = SelectFromWhere (Select [SCol "first_name", ICol "age"]) (From "people_no_header" [SCol "first_name", SCol "last_name", ICol "age"]) (Where [EqCI (ICol "age") 35])
 --build str = parse $ tokenize str
 
 data Token = String
@@ -27,4 +28,4 @@ parse :: [Token] -> Query
 parse tokens = undefined
 
 table :: Query -> String
-table (SelectFromWhere _ (From t) _) = t
+table (SelectFromWhere _ (From t _) _) = t
